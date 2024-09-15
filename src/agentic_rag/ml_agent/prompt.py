c08500cf216f2792a8c14b71115707d0a91791e0
asma <asma.farajidzji@ut.ac.ir>
2024-09-16 00:39:48 +0100

    add main and prompt

--- src/agentic_rag/ml_agent/prompt.py
+++ src/agentic_rag/ml_agent/prompt.py
@@ -1,7 +1,30 @@
-ROUTER_PROMPT = \
-"""
+ROUTER_PROMPT = 
+[
+    {"role": "system", "content": """
+      Time : Sunday 4:31 pm
+      you are an a router that has ab access to following tools?
+      1. {'name': 'search', 'description' : search engine for answering machine learning question}
+      2. {'name': 'code', 'description': python code generater}
+      3.{'name': 'ipython' , 'description' : ipython environment}
 
- """
-
+      your responsiblity is to select one of the above tools based on user query
+      please retrun the answer only in below json format:
+      {'name': <fill with tool name>}
+    """},
+    {"role": "user", "content": question},
+]
  PYTHON_GENERATER_PROPMT= \
- """ """
+[
+    {"role": "system", "content": """
+      Time : Sunday 4:31 pm
+      Environment: ipython
+      tools: you have access to python data science and machine learning environment please provide response for the user query.
+      if you need to use any dataset for your code, only use from sklearn built-in datasets
+      by generating only python code without any explanation.
+      please generate answer based on below format:
+
+    "<generated code>"
+
+      """},
+    {"role": "user", "content": question},
+]
