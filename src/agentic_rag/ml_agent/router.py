5c34a3d59151ab6d9868333ab99a2c02802a5f12
asma <asma.farajidzji@ut.ac.ir>
2024-09-16 00:27:35 +0100

    initialize router

---
+++ src/agentic_rag/ml_agent/router.py
@@ -0,0 +1,24 @@
+import requests
+URL = 'https://8c3a-34-87-63-89.ngrok-free.app/router'
+
+class Router:
+
+    def __init__():
+        
+    
+    def run(self, messeage):
+
+        agent_name = choose_agent(message)
+
+        if agent_name = 'code':
+                ...
+
+        elif :
+                ...
+    
+    def choose_agent(self, message):
+
+            r = requests.get(url = URL)
+            agent_name = r.json()['name']
+
+        return agent_name
